bd6ed549684d1420f4632130e3c1867123499f7a
codyasbin <himalthapa346@gmail.com>
2025-10-15 12:00:08 +0545

    first javascript class

---
+++ script.js
@@ -0,0 +1,72 @@
+// console.log("Welcome to javascript");
+// console.error("This is an error message");
+// console.warn("This is a warning message");
+// console.info("This is some information");
+
+
+// var name="sandesh";
+// let age=22;
+// const address="chitwan"
+
+// console.log("********initial values********");
+// console.log(name);
+// console.log(age);
+// console.log(address);
+
+// var name="sandeep";
+// name="utsab"
+
+// // let age=23;
+// age=25
+
+// address="kathmandu"
+
+// console.log("********after changing values********");
+// console.log(name);
+// console.log(age);
+// console.log(address);
+
+// console.log(a);
+// var a=10;
+
+
+
+// sayHi();
+
+// function sayHi() {
+//   console.log("Hi!");
+// };
+
+
+// let name="sandesh";
+// let number=4353534;
+// let isTrue=true;
+// let empty=null;
+// let notDefined=undefined;
+// let bigInt=BigInt(123456789012345678901234567890);
+// let symbol=Symbol("id");
+
+// console.log(typeof name);
+// console.log(typeof number);  
+// console.log(typeof isTrue);
+// console.log(typeof empty);
+// console.log(typeof notDefined);
+// console.log(typeof bigInt);
+// console.log(typeof symbol);       
+
+
+const person = {
+  name: "sandesh",
+  age: 22,
+  address: "chitwan"
+};
+
+const fruits = ["apple", "banana", "orange"];
+
+console.log(typeof person);
+console.log(typeof fruits);
+
+
+
+
+
