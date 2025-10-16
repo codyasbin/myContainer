64a6749a4df645ea0e79b1e1438b223f103db7a6
codyasbin <himalthapa346@gmail.com>
2025-10-16 12:27:47 +0545

    scripts added

--- script.js
+++ script.js
@@ -55,18 +55,86 @@
 // console.log(typeof symbol);       
 
 
-const person = {
-  name: "sandesh",
-  age: 22,
-  address: "chitwan"
-};
+// const person = {
+//   name: "sandesh",
+//   age: 22,
+//   address: "chitwan"
+// };
 
-const fruits = ["apple", "banana", "orange"];
+// const fruits = ["apple", "banana", "orange"];
 
-console.log(typeof person);
-console.log(typeof fruits);
-
-
+// console.log(typeof person);
+// console.log(typeof fruits);
 
 
+// String 
 
+// let firstName="sandesh";
+// let lastName="thapa";
+
+// console.log(`this human first name is ${firstName} and last name is ${lastName}`);
+
+// let TotalCount= firstName.length
+
+// console.log("The length of the first name is "+TotalCount);
+
+// console.log(firstName.charAt(3))
+
+// console.log(firstName.concat(" ",lastName))
+// console.log(firstName+" "+lastName)
+
+// let num=10
+// let num2="sjidfbkjsdnvs"
+
+// console.log(num*num2)
+
+
+// let parsedNum=parseInt(num2)
+// let parsedNUm2=Number(num2)
+
+// console.log(typeof parsedNum)
+// console.log(typeof parsedNUm2)
+
+
+// let parsedString=String(num)
+// console.log(typeof parsedString)
+
+// let str="javascript\n.   "
+
+// console.log(str.includes("sandesh"))
+
+// console.log(str.startsWith("Hello"))
+
+// console.log(str.endsWith("language."))
+
+// console.log(str.indexOf("a"))
+
+// console.log(str.lastIndexOf("a"))
+
+// console.log(str.slice(7,14))
+
+// str[0]="W"
+
+// console.log(str)
+
+// console.log(str.toUpperCase())
+
+// console.log(str.toLowerCase())
+
+// console.log(str.replace("JavaScript","JS"))
+
+// console.log(str.replaceAll("JavaScript","JS"))
+
+// console.log(str.trim())
+
+
+// console.log(str.padStart(20,"0"))
+
+// console.log(str.trim().repeat(10)
+// )
+
+let str="red%green%blue"
+
+console.log(str.split("%"))
+console.log(str.split("%",2))
+console.log(str.split(""))
